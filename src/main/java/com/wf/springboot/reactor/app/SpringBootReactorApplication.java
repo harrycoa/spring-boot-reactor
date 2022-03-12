88044043de2619249527c25efe80611a1f8048e7
Harry Coa <rodol_2000@hotmail.com>
2022-03-12 18:24:36 -0500

    feat:[reactor] Agregamos libreria coman line y dependencia reactor

--- src/main/java/com/wf/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/wf/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,18 @@
 package com.wf.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication  implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//Flux nombres =
+	}
 }
