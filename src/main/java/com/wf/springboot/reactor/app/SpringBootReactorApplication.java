1ff788ab58def17f8c4dead1bb18268ff39d9d15
Harry Coa <rodol_2000@hotmail.com>
2022-03-12 19:30:29 -0500

    feat:[reactor] ejemplo subscribe

--- src/main/java/com/wf/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/wf/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.wf.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,17 +10,27 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication  implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Andres", "Pedro", "Harry", "Yandira") // flux es un publisher (Observable)
+		Flux<String> nombres = Flux.just("Andres", "Pedro", "","Harry", "Yandira") // flux es un publisher (Observable)
 				//.doOnNext( element -> System.out.println(element) );
-				.doOnNext( System.out::println );
+				.doOnNext( e -> {
+					if (e.isEmpty()) {
+						throw new RuntimeException("Vacios");
+					} else {
+						System.out.println(e);
+					}
+				} );
 
-		nombres.subscribe();
+		// nombres.subscribe(log::info);
+		nombres.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage()));
 
 	}
 }
